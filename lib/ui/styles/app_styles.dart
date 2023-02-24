d290d1481b1115036ae11bca8c938a037c923922
supunnilakshana <supunnilakshana1999@gmail.com>
2023-02-24 22:07:28 +0530

    first commit

---
+++ lib/ui/styles/app_styles.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+const Color kPrimaryColorlight = Color.fromARGB(255, 251, 251, 251);
+const Color kPrimaryColordark = Color.fromARGB(255, 25, 139, 210);
+const double smallFont = 12.0;
+const double mediumFont = 16.0;
+const double largeFont = 24.0;
+
+const Color kBasefontColor = Colors.black;
+
+const kGradientButtonColor = LinearGradient(
+  colors: <Color>[
+    Color.fromARGB(255, 28, 161, 244),
+    Color.fromARGB(255, 25, 139, 210)
+  ],
+);
+
+const kGradientBLueColor = LinearGradient(
+  colors: <Color>[
+    Color.fromARGB(255, 28, 161, 244),
+    Color.fromARGB(255, 25, 139, 210)
+  ],
+);
+const kGradientGreen = LinearGradient(begin: Alignment.topCenter, colors: [
+  Color.fromARGB(255, 28, 161, 244),
+  Color.fromARGB(255, 25, 139, 210),
+  Color.fromARGB(255, 12, 96, 160),
+]);
---
+++ lib/ui/widgets/popup_dilog.dart
@@ -0,0 +1,141 @@
+import 'package:awesome_dialog/awesome_dialog.dart';
+import 'package:clinicapp/ui/styles/app_styles.dart';
+
+import 'package:flutter/material.dart';
+
+class PopupDialog {
+  static showPopupDilog(BuildContext context, String titel, String description,
+      Function actionFun) {
+    AwesomeDialog(
+            context: context,
+            dialogType: DialogType.INFO,
+            animType: AnimType.BOTTOMSLIDE,
+            title: titel,
+            desc: description,
+            btnCancelText: "Cancel",
+            btnOkText: "Join",
+            btnCancelOnPress: () {},
+            btnOkOnPress: () {
+              actionFun();
+            },
+            btnOkColor: kPrimaryColordark,
+            btnCancelColor: Colors.grey)
+        .show();
+  }
+
+  static showPopupWarning(BuildContext context, String titel,
+      String description, Function actionFun) {
+    AwesomeDialog(
+      context: context,
+      dialogType: DialogType.WARNING,
+      animType: AnimType.BOTTOMSLIDE,
+      title: titel,
+      desc: description,
+      btnCancelText: "No",
+      btnOkText: "Yes",
+      btnCancelOnPress: () {},
+      btnOkOnPress: actionFun(),
+    ).show();
+  }
+
+  static showPopuplogout(BuildContext context, String titel, String description,
+      Function actionFun) {
+    AwesomeDialog(
+      context: context,
+      dialogType: DialogType.WARNING,
+      animType: AnimType.BOTTOMSLIDE,
+      title: titel,
+      desc: description,
+      btnCancelText: "No",
+      btnOkText: "Yes",
+      btnCancelOnPress: () {},
+      btnOkOnPress: () {
+        actionFun();
+      },
+    ).show();
+  }
+
+  static showPopupErorr(
+    BuildContext context,
+    String titel,
+    String description,
+  ) {
+    AwesomeDialog(
+      context: context,
+      dialogType: DialogType.ERROR,
+      animType: AnimType.BOTTOMSLIDE,
+      title: titel,
+      desc: description,
+      btnOkText: "Ok",
+      btnCancel: null,
+      btnOkOnPress: () {},
+    ).show();
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*static Future<void> showMyDialog(BuildContext context, String titel,
+      String description, Function actionFun) async {
+    return showDialog<void>(
+      context: context,
+      barrierDismissible: false, // user must tap button!
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text(titel),
+          content: SingleChildScrollView(
+            child: ListBody(
+              children: <Widget>[
+                Text(description),
+              ],
+            ),
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: const Text('Yes , did'),
+              onPressed: () {
+                /// actionFun();
+                AwesomeDialog(
+                  context: context,
+                  dialogType: DialogType.INFO,
+                  animType: AnimType.LEFTSLIDE,
+                  title: 'Dialog Title',
+                  desc: 'Dialog description here.............',
+                  btnCancelOnPress: () {},
+                  btnOkOnPress: () {},
+                )..show();
+              },
+            ),
+            TextButton(
+              child: const Text('Not yet'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  } */
